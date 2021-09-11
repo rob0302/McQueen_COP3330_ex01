50f70d1b3411c1d53bce25d67ac908970801c2dd
Robert McQueen <robertmcqueen@Roberts-iMac.local>
2021-09-10 23:07:50 -0400

    Initial commit

---
+++ src/main/java/org/example/SayingHello.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.*;
+
+public class SayingHello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+String name = sc.nextLine();
+System.out.print("Hello, "+ name +", Nice to meet you!");
+
+    }
+}
